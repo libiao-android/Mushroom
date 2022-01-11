142112a8ca9f87f29f7e445573d7b5d49c1fba6c
libiao <297946329@qq.com>
2022-01-11 09:23:45 +0800

    feat: 放量策略调整

--- app/src/main/java/com/libiao/mushroom/mode/MoreMore3Mode.kt
+++ app/src/main/java/com/libiao/mushroom/mode/MoreMore3Mode.kt
@@ -2,6 +2,7 @@ package com.libiao.mushroom.mode
 
 import com.libiao.mushroom.SharesRecordActivity
 import com.libiao.mushroom.utils.Constant
+import com.libiao.mushroom.utils.LogUtil
 import com.libiao.mushroom.utils.LogUtil.i
 
 class MoreMore3Mode : BaseMode {
@@ -23,48 +24,67 @@ class MoreMore3Mode : BaseMode {
             val one = shares[mDeviationValue + 0]
             val two = shares[mDeviationValue + 1]
             val three = shares[mDeviationValue + 2]
-            val four = shares[mDeviationValue + 3]
-            val five = shares[mDeviationValue + 4]
 
-            var maxT = one.totalPrice
-            if(two.totalPrice > maxT) maxT = two.totalPrice
-            if(three.totalPrice > maxT) maxT = three.totalPrice
-            if(four.totalPrice > maxT) maxT = four.totalPrice
-            if(five.totalPrice > maxT) maxT = five.totalPrice
+            val ten1 = shares[mDeviationValue + 3]
+            val ten2 = shares[mDeviationValue + 4]
+            val ten3 = shares[mDeviationValue + 5]
+            val ten4 = shares[mDeviationValue + 6]
+            val ten5 = shares[mDeviationValue + 7]
 
+            if(one.code == "sz000558") {
+                LogUtil.i(TAG, "${one.code}")
+            }
+            var preAvg = (one.totalPrice + two.totalPrice + three.totalPrice) / 3
+            if(zhangTing(three)) {
+                preAvg = (one.totalPrice + two.totalPrice) / 2
+            }
 
-            val ten1 = shares[mDeviationValue + 5]
-            val ten2 = shares[mDeviationValue + 6]
-            val ten3 = shares[mDeviationValue + 7]
-
-
-            val preAvg = maxT
-
-            var beiShu = 4.0
-            if(preAvg > 2000000000) {
+            var beiShu = 3.0
+            if(preAvg > 1000000000) {
                 beiShu = 2.0
-            } else if(preAvg > 1000000000) {
-                beiShu = 2.5
             } else if(preAvg > 500000000) {
-                beiShu = 3.0
+                beiShu = 2.5
             } else if(preAvg > 100000000) {
-                beiShu = 3.5
+                beiShu = 3.0
             }
 
             val moreAvg = preAvg * beiShu
 
+            if(moreAvg > 0
+                && (ten1.totalPrice > moreAvg)
+                && (ten2.totalPrice > moreAvg)
+                && ten3.totalPrice > moreAvg
+                && ten4.totalPrice > moreAvg
+                && ten5.totalPrice > moreAvg
+            ) {
 
-            if(moreAvg > 0 && ten1.totalPrice > moreAvg && ten2.totalPrice > moreAvg && ten3.totalPrice > moreAvg) {
-
-                if(ten3.totalPrice > 100000000) {
-                    if(ten2.totalPrice > ten1.totalPrice * 0.75) {
-                        if(ten3.totalPrice > ten2.totalPrice * 0.75) {
+                if(ten5.totalPrice > 100000000) {
+                    if(!(ten3.totalPrice > ten4.totalPrice && ten4.totalPrice > ten5.totalPrice)) {
+                        if(ten5.nowPrice > ten1.beginPrice) {
                             i(TAG, "${ten3.brieflyInfo()}, $preAvg")
-                            val avg1 = (ten1.totalPrice + ten2.totalPrice + ten3.totalPrice) / 3
-                            val avg2 = (one.totalPrice + two.totalPrice + three.totalPrice + four.totalPrice + five.totalPrice) / 5
-                            ten3.post1 = String.format("%.1f",avg1 / avg2)
-                            ten3.post2 = String.format("%.1f",(ten1.range + ten2.range + ten3.range))
-                            mFitModeList.add(Pair(ten3.range, ten3))
+
+                            var max = ten1.totalPrice
+                            var min = ten1.totalPrice
+                            if(zhangTing(ten1)) min = ten2.totalPrice
+                            if(ten2.totalPrice > max) max = ten2.totalPrice
+                            if(ten3.totalPrice > max) max = ten3.totalPrice
+                            if(ten4.totalPrice > max) max = ten4.totalPrice
+                            if(ten5.totalPrice > max) max = ten5.totalPrice
+
+                            if(ten2.totalPrice < min) min = ten2.totalPrice
+                            if(ten3.totalPrice < min) min = ten3.totalPrice
+                            if(ten4.totalPrice < min) min = ten4.totalPrice
+                            if(ten5.totalPrice < min) min = ten5.totalPrice
+
+                            val liangBi = max / min
+
+                            val avg1 = (ten1.totalPrice + ten2.totalPrice + ten3.totalPrice
+                                    + ten4.totalPrice + ten5.totalPrice) / 5
+                            val avg2 = one.totalPrice
+                            ten5.post1 = String.format("%.1f",avg1 / avg2)
+                            //ten5.post2 = String.format("%.1f",(ten1.range + ten2.range + ten3.range + ten4.range + ten5.range))
+                            ten5.post2 = String.format("%.1f",liangBi)
+                            mFitModeList.add(Pair(ten5.range, ten5))
                         }
                     }
                 }
